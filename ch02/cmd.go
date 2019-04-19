be1030b7873bb4b621d41fa40be9ff03cab0dbc8
yuqiu314 <yuqiu314@hotmail.com>
2019-04-19 08:26:24 +0800

    init commit, in ch02

---
+++ ch02/cmd.go
@@ -0,0 +1,36 @@
+package main
+
+import "flag"
+import "fmt"
+import "os"
+
+type Cmd struct {
+	helpFlag    bool
+	versionFlag bool
+	cpOption    string
+	XjreOption  string
+	class       string
+	args        []string
+}
+
+func parseCmd() *Cmd {
+	cmd := &Cmd{}
+	flag.Usage = printUsage
+	flag.BoolVar(&cmd.helpFlag, "Help", false, "print help message")
+	flag.BoolVar(&cmd.helpFlag, "?", false, "print help message")
+	flag.BoolVar(&cmd.versionFlag, "version", false, "print version and exit")
+	flag.StringVar(&cmd.cpOption, "classpath", "", "class parth")
+	flag.StringVar(&cmd.cpOption, "cp", "", "class path")
+	flag.StringVar(&cmd.XjreOption, "Xjre", "", "path to jre")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) > 0 {
+		cmd.class = args[0]
+		cmd.args = args[1:]
+	}
+	return cmd
+}
+
+func printUsage() {
+	fmt.Printf("Usage: %s [-options] class [args...]\n", os.Args[0])
+}
